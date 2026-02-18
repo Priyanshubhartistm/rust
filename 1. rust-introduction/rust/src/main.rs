fc665444f9cb5f55e0efcfd93ae1d0cb567afaaf
Priyanshubhartistm <bhartipriyanshustm@gmail.com>
2026-02-19 00:33:01 +0530

    Initial commit: Rust learning projects

---
+++ 1. rust-introduction/rust/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!("Hello, world Priya!");
+            println!("Hello, world Priya!");
+            
+    
+}
---
+++ 2.guessing-game/guessing-game/src/main.rs
@@ -0,0 +1,21 @@
+use std::io;
+use rand::Rng;
+
+
+fn main() {
+    println!("guess the number!");
+
+    let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
+    println!("generated secret number{secret_number}");
+
+
+
+    print!("Please input your guess: ");
+
+    let mut guess: String = String::new();
+    
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
+
+}
