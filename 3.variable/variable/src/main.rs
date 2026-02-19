ed0d125737a44a7da181f4a9163e1f80685147ba
Priyanshubhartistm <bhartipriyanshustm@gmail.com>
2026-02-19 15:33:46 +0530

    var

--- 3.variable/variable/src/main.rs
+++ 3.variable/variable/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+
+    // let mut age: u32 = 30;
+    // age = 24;
+    // println!("my age is {}", age);
+
+    // constant
+    // const PI: u8 = 10;
+    // println!("PI value is {}", PI);
+    
+    // shadowing
+    let apples: i32 = 10;
+    println!("apples{apples}");
+
+    let apples: i32 = apples + 20;
+    println!("apples{apples}");
 }
